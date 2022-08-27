fb6d57f812377e0f9deadeb1f0d34db9f2d91e1d
Christopher Sarmiento <chrisar11@gmail.com>
2022-08-26 22:33:47 -0700

    commit tree dddcb4dc3b306a44c608d34ba719f44ce16f58b1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    updateText()
+    function updateText() {
+        document.querySelector('#text')
+        .innerText - 'This is really cool!';
+    }
+});
